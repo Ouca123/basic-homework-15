febe75574df1f61266dbcd870baa20ff9969ae9d
Ouca123 <48576489+Ouca123@users.noreply.github.com>
2019-03-29 13:55:10 +0800

    Add files via upload

---
+++ 作業15.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+int main()
+{
+ int H,M; 
+ printf("請輸入現在時間(24小時制):"); 
+ scanf("%d%d",&H,&M);
+ int HtoM=H*60;
+  
+ if(HtoM+M>=450&&HtoM+M<=1020) 
+ {
+   printf("現在是上課時間");
+ }
+ else
+ {
+  printf("現在是下課時間");
+ }
+
+}
